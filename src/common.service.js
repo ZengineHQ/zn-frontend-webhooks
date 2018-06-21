c356becee6129f583fe1799b6857090b8ccfe4c0
Alex Weber <alexweber15@gmail.com>
2018-06-21 12:47:00 -0300

    style: formatting

--- src/common.service.js
+++ src/common.service.js
@@ -1,106 +1,106 @@
 plugin.service('wgnWebhookCommon', ['$routeParams', 'znData', function ($routeParams, znData) {
-	var srv = this;
-	var workspaceId = $routeParams.workspace_id;
+  var srv = this;
+  var workspaceId = $routeParams.workspace_id;
 
-	/**
-	 * Creates a new webhook.
-	 *
-	 * @param {Object} params Default params for scheduled/non-scheduled.
-	 * @param {Object} options User params
-	 * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
-	 *
-	 * @return {Promise<Object>}
-	 */
-	srv.create = function (params, options, scheduled) {
+  /**
+   * Creates a new webhook.
+   *
+   * @param {Object} params Default params for scheduled/non-scheduled.
+   * @param {Object} options User params
+   * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
+   *
+   * @return {Promise<Object>}
+   */
+  srv.create = function (params, options, scheduled) {
     /*jshint maxcomplexity:9 */
-		scheduled = scheduled || false;
-		params = params || {};
+    scheduled = scheduled || false;
+    params = params || {};
 
-		var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
+    var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
 
-		if (!options || typeof options !== 'object') {
-			throw new Error('Options required.');
-		}
+    if (!options || typeof options !== 'object') {
+      throw new Error('Options required.');
+    }
 
-		angular.extend(params, options, { 'workspace.id': workspaceId });
+    angular.extend(params, options, { 'workspace.id': workspaceId });
 
-		if (!('scope' in params) || !params.scope) {
-			params.scope = 'plugin.wgn';
-		}
+    if (!('scope' in params) || !params.scope) {
+      params.scope = 'plugin.wgn';
+    }
 
-		// Check for a backend service endpoint suffix.
-		if (params.url.indexOf('http') !== 0) {
-			params.url = 'https://plugins.zenginehq.com/workspaces/' + workspaceId + '/wgn/' + params.url;
-		}
+    // Check for a backend service endpoint suffix.
+    if (params.url.indexOf('http') !== 0) {
+      params.url = 'https://plugins.zenginehq.com/workspaces/' + workspaceId + '/wgn/' + params.url;
+    }
 
-		return znData(resource).save(params);
-	};
+    return znData(resource).save(params);
+  };
 
-	/**
-	 * Updates a webhook.
-	 *
-	 * @param {Object} data
-	 * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
-	 *
-	 * @return {Promise<Object>}
-	 */
-	srv.update = function (data, scheduled) {
-		var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
-		return znData(resource).save({ id: data.id }, data);
-	};
+  /**
+   * Updates a webhook.
+   *
+   * @param {Object} data
+   * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
+   *
+   * @return {Promise<Object>}
+   */
+  srv.update = function (data, scheduled) {
+    var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
+    return znData(resource).save({ id: data.id }, data);
+  };
 
-	/**
-	 * Disables a webhook.
-	 *
-	 * @param {number} webhookId
-	 * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
-	 *
-	 * @return {Promise<Object>}
-	 */
-	srv.disable = function (webhookId, scheduled) {
-		return toggle(webhookId, false, scheduled);
-	};
+  /**
+   * Disables a webhook.
+   *
+   * @param {number} webhookId
+   * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
+   *
+   * @return {Promise<Object>}
+   */
+  srv.disable = function (webhookId, scheduled) {
+    return toggle(webhookId, false, scheduled);
+  };
 
-	/**
-	 * Enables a webhook.
-	 *
-	 * @param {number} webhookId
-	 * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
-	 *
-	 * @return {Promise<Object>}
-	 */
-	srv.enable = function (webhookId, scheduled) {
-		return toggle(webhookId, true, scheduled);
-	};
+  /**
+   * Enables a webhook.
+   *
+   * @param {number} webhookId
+   * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
+   *
+   * @return {Promise<Object>}
+   */
+  srv.enable = function (webhookId, scheduled) {
+    return toggle(webhookId, true, scheduled);
+  };
 
-	/**
-	 * Deletes a webhook.
-	 *
-	 * @param {number} webhookId
-	 * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
-	 *
-	 * @return {Promise<Object>}
-	 */
-	srv.delete = function (webhookId, scheduled) {
-		var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
-		return znData(resource).delete({ id: webhookId });
-	};
+  /**
+   * Deletes a webhook.
+   *
+   * @param {number} webhookId
+   * @param {boolean} scheduled Whether we're dealing with Scheduled Webhooks, defaults to regular ones.
+   *
+   * @return {Promise<Object>}
+   */
+  srv.delete = function (webhookId, scheduled) {
+    var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
+    return znData(resource).delete({ id: webhookId });
+  };
 
-	/**
-	 * Saves the 'isActive' flag for webhooks.
-	 *
-	 * @param {number} webhookId
-	 * @param {boolean} enable
-	 * @param {boolean} scheduled
-	 */
-	function toggle (webhookId, enable, scheduled) {
-		enable = enable || false;
-		scheduled = scheduled || false;
+  /**
+   * Saves the 'isActive' flag for webhooks.
+   *
+   * @param {number} webhookId
+   * @param {boolean} enable
+   * @param {boolean} scheduled
+   */
+  function toggle (webhookId, enable, scheduled) {
+    enable = enable || false;
+    scheduled = scheduled || false;
 
-		var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
-		return znData(resource).save({ id: webhookId }, { isActive: !!enable });
-	}
+    var resource = scheduled ? 'ScheduledWebhooks' : 'Webhooks';
+    return znData(resource).save({ id: webhookId }, { isActive: !!enable });
+  }
 
-	return srv;
+  return srv;
 
 }]);
